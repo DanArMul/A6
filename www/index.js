13f77fce803444133ad2fc90b0bb4eda4279858a
Daniel Muljono <dmuljono@ucsd.edu>
2021-02-24 17:26:39 +0700

    Website proto 0.1

---
+++ www/index.js
@@ -0,0 +1,12 @@
+const login = () => {
+  const username = document.querySelector('#username').value;
+  const password = document.querySelector('#password').value;
+
+  if (username === 'user' && password === 'pass') {
+    // call the navigator to move to the new page
+    const navigator = document.querySelector('#navigator');
+    navigator.resetToPage('home.html'); 
+  } else {
+    ons.notification.alert('Wrong username/password combination');
+  }
+}
